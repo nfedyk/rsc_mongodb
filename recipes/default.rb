62dd30089f82b634026d95fe497e56e5543199ce
gonzalez <edds718@gmail.com>
2015-08-20 09:24:05 -0400

    include_recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,5 +16,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include 'mongodb::10gen_repo'
-include 'mongodb:default'
+include_recipe 'mongodb::10gen_repo'
+include_recipe 'mongodb:default'
