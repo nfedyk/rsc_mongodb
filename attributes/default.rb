9280e3e6573ca0683d53eae5f336379e0e5d6144
gonzalez <edds718@gmail.com>
2015-09-10 13:26:04 -0400

    adding replicaset recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 node.default['mongodb']['cluster_name'] = 'rs_my_replicaset'
-node.default['mongodb']['config']['replSet'] = 'rs_my_replicaset'
+node.default['mongodb']['config']['replset'] = 'rs_my_replicaset'
 node.default['mongodb']['replicaset_name'] = 'rs_my_replicaset'
