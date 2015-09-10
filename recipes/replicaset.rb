ab9ad2ee802fe5d59cd576cb1f08f41b8932526f
gonzalez <edds718@gmail.com>
2015-09-10 16:22:47 -0400

    finding members of the replicaset

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -5,20 +5,21 @@ end
 replicaset_members = []
 
 sets = tag_search(node, "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}")
+puts sets
 
-sets.each do | set |
-
-	ip_address = set['server:private_0'].first.value + ':27018'
-
-	members << {
-		:host => ip_address
-  	}
-end
-
-replicaset_members.sort! { |a,b| a[:host] <=> b[:host] }
-
-Chef::Log.info "replicaset members " + replicaset_members.inspect
-
-mongodb_replicaset node['mongodb']['replicaset'] do
-  members members
-end
+# sets.each do | set |
+#
+# 	ip_address = set['server:private_0'].first.value + ':27018'
+#
+# 	members << {
+# 		:host => ip_address
+#   	}
+# end
+#
+# replicaset_members.sort! { |a,b| a[:host] <=> b[:host] }
+#
+# Chef::Log.info "replicaset members " + replicaset_members.inspect
+#
+# mongodb_replicaset node['mongodb']['replicaset'] do
+#   members members
+# end
