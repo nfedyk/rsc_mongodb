8e1a5f8f0fe88c7a4011c1000a0fd34f200c5206
gonzalez <edds718@gmail.com>
2015-09-10 15:14:03 -0400

    removing build-essential

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-node.default[:build-essential][:compile_time] = true
+#node.default[:build-essential][:compile_time] = true
 node.default[:mongodb][:config][:replset] = #{node[:rsc_mongodb][:replicaset]}
 #include_recipe 'build-essential::default'
 include_recipe 'mongodb::mongodb_org_repo'
