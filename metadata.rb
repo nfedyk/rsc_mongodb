30a6b23449d536de71075a22e5ebfae4f4dc258d
gonzalez <edds718@gmail.com>
2015-09-10 15:03:50 -0400

    14.04 issue with build-essentials

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures Mongo DB'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-#depends 'build-essential'
+depends 'build-essential'
 depends 'mongodb'
 depends 'machine_tag'
 
