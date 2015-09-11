9582bd93b8d5ee7ab513d312f79ba7126c22a691
gonzalez <edds718@gmail.com>
2015-09-11 13:02:39 -0400

    mongo shell js commands

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -11,7 +11,7 @@ replicaset_hosts = tag_search(node, "mongodb:replicaset=#{node[:rsc_mongodb][:re
 
 replicaset_hosts.each do | server |
 #
-   ip_address = server['server:private_ip_0'].first.value + ':27018'
+   ip_address = server['server:private_ip_0'].first.value + ':27017'
    Chef::Log.info "#{ip_address}"
 
  	 replicaset_members << {
@@ -19,7 +19,21 @@ replicaset_hosts.each do | server |
    	}
 end
 
-node.set['mongodb']['is_replicaset'] = true
-node.set['mongodb']['cluster_name'] = node['mongodb']['cluster_name']
+
 
 #initialize replicaset
+## initiate replica set
+bash 'configure the replicaset' do
+  code <<-EOF
+    mongo --quiet <<EOF
+    rs.initiate({
+      _id: '#{node[:rsc_mongodb][:replicaset]}',
+      members: [
+        {_id: 0, host: '10.180.214.2:27017'},
+        {_id: 1, host: '10.237.182.130:27017'},
+        {_id: 2, host: '10.65.181.158:27017'}
+        ]
+      });
+    EOF
+  EOF
+end
