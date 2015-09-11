5e1ae5624553c3799584ddcf523eda3a6ea3a702
gonzalez <edds718@gmail.com>
2015-09-11 10:13:37 -0400

    join the 3 nodes into a replicaset

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -23,6 +23,6 @@ end
 #
 # Chef::Log.info "replicaset members " + replicaset_members.inspect
 #
-# mongodb_replicaset node['mongodb']['replicaset'] do
-#   members members
-# end
+mongodb_replicaset node['mongodb']['replicaset'] do
+   members replicaset_members
+end
