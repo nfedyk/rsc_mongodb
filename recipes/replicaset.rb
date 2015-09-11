9cb767bff952ffae953d928baa3ad9f208e92a92
gonzalez <edds718@gmail.com>
2015-09-11 10:32:47 -0400

    using mongodb_instance to set replicaset

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -18,11 +18,18 @@ replicaset_hosts.each do | server |
  		:host => ip_address
    	}
 end
-#
-# replicaset_members.sort! { |a,b| a[:host] <=> b[:host] }
-#
-# Chef::Log.info "replicaset members " + replicaset_members.inspect
-#
-mongodb_replicaset node['mongodb']['replicaset'] do
-   members replicaset_members
-end
+
+node.set['mongodb']['is_replicaset'] = true
+node.set['mongodb']['cluster_name'] = node['mongodb']['cluster_name']
+
+include_recipe 'mongodb::mongo_gem'
+
+  mongodb_instance node['mongodb']['instance_name'] do
+    mongodb_type 'mongod'
+    port         node['mongodb']['config']['port']
+    logpath      node['mongodb']['config']['logpath']
+    dbpath       node['mongodb']['config']['dbpath']
+    replicaset   node
+    enable_rest  node['mongodb']['config']['rest']
+    smallfiles   node['mongodb']['config']['smallfiles']
+  end
