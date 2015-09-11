5cdb63376bbce855375b830b391aa2c943a14b1f
gonzalez <edds718@gmail.com>
2015-09-11 13:47:15 -0400

    add node to replicaset

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -20,13 +20,16 @@ replicaset_hosts = tag_search(node, "mongodb:replicaset=#{node[:rsc_mongodb][:re
 
 
 replicaset_hosts.each do | server |
-#
+
    ip_address = server['server:private_ip_0'].first.value + ':27017'
    Chef::Log.info "#{ip_address}"
+   execute 'add node to replicaset' do
+     command "mongo --quiet <<EOF rs.add(#{ip_address});EOF"
+   end
 
- 	 replicaset_members << {
- 		:host => ip_address
-   	}
+ # 	 replicaset_members << {
+ # 		:host => ip_address
+  #  	}
 end
 
 
