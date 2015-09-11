ba064c9a9a280df16e22ebd8da816e258e94846e
gonzalez <edds718@gmail.com>
2015-09-11 14:46:01 -0400

    fix end of line error

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -29,27 +29,6 @@ replicaset_hosts.each do | server |
           rs.add(#{ip_address});
         EOF
       EOH
+    end
 
- # 	 replicaset_members << {
- # 		:host => ip_address
-  #  	}
 end
-
-
-
-# #initialize replicaset
-# ## initiate replica set
-# bash 'configure the replicaset' do
-#   code <<-EOH
-#     mongo --quiet <<EOF
-#     rs.initiate({
-#       _id: '#{node[:rsc_mongodb][:replicaset]}',
-#       members: [
-#         {_id: 0, host: '10.180.214.2:27017'},
-#         {_id: 1, host: '10.237.182.130:27017'},
-#         {_id: 2, host: '10.65.181.158:27017'}
-#         ]
-#       });
-#     EOF
-#   EOH
-# end
