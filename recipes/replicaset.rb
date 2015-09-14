2d1a7e618df0709a6367da068a599b99b99b5c9c
gonzalez <edds718@gmail.com>
2015-09-14 14:09:00 -0400

    fixing js syntax

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -19,7 +19,7 @@ replicaset_hosts.each do | server |
 
    ip_address = server['server:private_ip_0'].first.value + ':27017'
    Chef::Log.info "#{ip_address}"
-   rs_config = rs_config.to_s + "{_id : #{host_id}, host : #{ip_address}},"
+   rs_config = rs_config.to_s + "{_id: #{host_id}, host: #{ip_address}},"
    host_id += 1
   #  bash 'add node to replicaset' do
   #    code <<-EOH
