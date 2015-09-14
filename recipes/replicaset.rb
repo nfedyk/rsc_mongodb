76ead1a36d0daf5e20a35453a5f1156416f11a4f
gonzalez <edds718@gmail.com>
2015-09-14 12:58:12 -0400

    generating config file to pass into rs.initiate

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -2,15 +2,6 @@ class Chef::Recipe
   include Chef::MachineTagHelper
 end
 
-## initiate replica set , replica set name is already in the config
-bash 'initiate the node' do
-  code <<-EOH
-    mongo --quiet <<EOF
-    rs.initiate();
-    EOF
-  EOH
-end
-
 include_recipe 'machine_tag::default'
 
 replicaset_members = []
@@ -18,16 +9,35 @@ Chef::Log.info 'Searching for mongodb nodes'
 
 replicaset_hosts = tag_search(node, "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}")
 
+host_id=0
+rs_config = "config = {
+    _id : mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]},
+     members : ["
+
 
 replicaset_hosts.each do | server |
 
    ip_address = server['server:private_ip_0'].first.value + ':27017'
    Chef::Log.info "#{ip_address}"
-   bash 'add node to replicaset' do
-     code <<-EOH
-        mongo --quiet <<EOF
-          rs.add("#{ip_address}");
-        EOF
-      EOH
-    end
+   rs_config = rs_config.to_s + "{_id : #{host_id}, host : #{ip_address}},"
+   host_id += 1
+  #  bash 'add node to replicaset' do
+  #    code <<-EOH
+  #       mongo --quiet <<EOF
+  #         rs.add("#{ip_address}");
+  #       EOF
+  #     EOH
+  #   end
+end
+rs_config = rs_config.to_s + "     ]
+}"
+
+
+## initiate replica set , replica set name is already in the config
+bash 'initiate the node' do
+  code <<-EOH
+    mongo --quiet <<EOF
+    rs.initiate(#{rs_config}.to_s);
+    EOF
+  EOH
 end
