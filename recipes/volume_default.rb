3a4ff8c5b2e75649a53553e17f57b7fdc2150331
gonzalez <edds718@gmail.com>
2015-09-16 15:50:46 -0400

    create attached and format

--- recipes/volume_default.rb
+++ recipes/volume_default.rb
@@ -1,7 +1,22 @@
 
 include_recipe 'rightscale_volume::default'
 
-rightscale_volume "db_data_volume" do
+rightscale_volume "mongo_data_volume" do
   size 10
   action :create
+end
+
+# Attaches the volume to the instance
+rightscale_volume "mongo_data_volume" do
+  action :attach
+end
+
+execute "format volume as ext4" do
+  command lazy { "mkfs.ext4 #{node['rightscale_volume']['db_data_volume']['device']}" }
+  action :run
+end
+
+execute "mount volume to /mnt/storage" do
+  command lazy { "mkdir -p /mnt/storage; mount #{node['rightscale_volume']['db_data_volume']['device']} /mnt/storage" }
+  action :run
 end
