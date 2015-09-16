9d7cb4fc3fff0e5a9d3ec8dfdbf905deefdf855a
gonzalez <edds718@gmail.com>
2015-09-16 08:47:47 -0400

    new branch to use volume support

---
+++ recipes/add_to_replicaset.rb
@@ -0,0 +1,30 @@
+class Chef::Recipe
+  include Chef::MachineTagHelper
+end
+
+include_recipe 'machine_tag::default'
+
+Chef::Log.info 'Searching a mongodb node'
+
+mongo_node = tag_search(node, "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}")
+
+
+   ip_address = mongo_node['server:private_ip_0'].first.value + ':27017'
+   Chef::Log.info "#{ip_address}"
+   rs_config = rs_config.to_s + "{_id: #{host_id}, host: \'#{ip_address}\'},"
+   host_id += 1
+
+end
+rs_config = rs_config.to_s + "     ]
+}"
+
+Chef::Log.info "#{rs_config}"
+## initiate replica set , replica set name is already in the config
+bash 'initiate the node' do
+  code <<-EOH
+    mongo <<CONFIG
+      rs.initiate(#{rs_config}.to_s);
+    CONFIG
+  EOH
+  flags '-xe'
+end
