b5816bce7b49c05797b9155f56af67e729912692
gonzalez <edds718@gmail.com>
2015-09-17 16:36:21 -0400

    add category

--- metadata.rb
+++ metadata.rb
@@ -21,6 +21,7 @@ attribute 'rsc_mongodb/replicaset',
    :display_name => 'MongoDB ReplicaSet Name',
    :description => 'The replicaset name to use for the mongodb replica',
    :required => 'required',
+   :category => 'MongoDB',
    :type => 'string',
    :recipes => ['rsc_mongodb::default', 'rsc_mongodb::replicaset']
 
@@ -28,6 +29,7 @@ attribute 'rsc_mongodb/use_storage',
   :display_name => 'Enable volumes',
   :description => 'Enables the use of volumes for the Mongodb datadir',
   :required => 'optional',
+  :category => 'MongoDB',
   :default => 'false'
   :type => 'string',
   :recipes => ['rsc_mongodb::volume_default']
@@ -36,6 +38,7 @@ attribute 'rsc_mongodb/volumne_nickname',
   :display_name => 'Volume Name',
   :description => 'Name of the volume',
   :required => 'optional',
+  :category => 'MongoDB',
   :default => 'mongodb_data_volume'
   :type => 'string',
   :recipes => ['rsc_mongodb::volume_default']
@@ -44,6 +47,7 @@ attribute 'rsc_mongodb/volumne_size',
   :display_name => 'Volume Size',
   :description => 'the size of the volume to create',
   :required => 'optional',
+  :category => 'MongoDB',
   :default => '10'
   :type => 'string',
   :recipes => ['rsc_mongodb::volume_default'
@@ -52,6 +56,7 @@ attribute 'rsc_mongodb/volumne_filesystem',
   :display_name => 'Volume Filesystem',
   :description => 'the filesystem size',
   :required => 'optional',
+  :category => 'MongoDB',
   :default => 'ext4'
   :type => 'string',
   :recipes => ['rsc_mongodb::volume_default'
@@ -60,6 +65,7 @@ attribute 'rsc_mongodb/mount_point',
   :display_name => 'Volume Mount Point ',
   :description => 'the location to mount the volume',
   :required => 'optional',
+  :category => 'MongoDB',
   :default => '/mnt/mongodb'
   :type => 'string',
   :recipes => ['rsc_mongodb::volume_default'
