8973bd618ad82ad5eec293dca2af18c2938a8b71
gonzalez <edds718@gmail.com>
2015-09-18 15:58:03 -0400

    adding wrapper for rs-storage::backup

--- recipes/mongodb_backup.rb
+++ recipes/mongodb_backup.rb
@@ -1,5 +1,6 @@
 node.override['rs-storage']['device']['mount_point'] = "#{node['rsc_mongodb']['volume_mount_point']}"
-node.override['rs-storage']['backup']['lineage'] = "#{node['rsc_mongodb']['backup_lineage_name']}"
+node.override['rs-storage']['backup']['lineage'] = 'blahblah'
+#node.override['rs-storage']['backup']['lineage'] = "#{node['rsc_mongodb']['backup_lineage_name']}"
 
 Chef::Log.info "Backup Lineage: #{node['rsc_mongodb']['backup_lineage_name']}"
 
