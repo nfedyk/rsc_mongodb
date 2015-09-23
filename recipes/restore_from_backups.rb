3d716e0f02bae40d5e71fefb4e5e032b92dc93c3
gonzalez <edds718@gmail.com>
2015-09-23 16:47:40 -0400

    adding restore volume recipe

---
+++ recipes/restore_from_backups.rb
@@ -0,0 +1,15 @@
+
+node.default['rs-storage']['device']['nickname'] = "#{node['rsc_mongodb']['volume_nickname']}"
+node.default['rs-storage']['device']['volume_size'] = "#{node['rsc_mongodb']['volume_size']}"
+node.default['rs-storage']['device']['filesystem'] = "#{node['rsc_mongodb']['volume_filesystem']}"
+node.default['rs-storage']['device']['mount_point'] = "#{node['rsc_mongodb']['volume_mount_point']}"
+
+#installs right_api_client
+include_recipe 'rightscale_volume::default'
+
+include_recipe 'rs-storage::volume'
+
+#when using volumes we set the datadir to the mount point.
+node.default[:mongodb][:config][:datadir] = "#{node.default['rs-storage']['device']['mount_point']}"
+
+node.default[:rs-storage][:restore][:lineage] = "#{node['rsc_mongodb']['restore_lineage_name']}"
