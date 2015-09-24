f543aacb71c037204796ac99d794cb014580ae57
gonzalez <edds718@gmail.com>
2015-09-24 13:42:11 -0400

    changing order of external recipes

--- recipes/volume_default.rb
+++ recipes/volume_default.rb
@@ -28,6 +28,6 @@ include_recipe 'rightscale_volume::default'
 include_recipe 'rs-storage::volume'
 
 #when using volumes we set the datadir to the mount point.
-node.default[:mongodb][:config][:datadir] = "#{node.default['rs-storage']['device']['mount_point']}"
+node.default[:mongodb][:config][:dbPath] = "#{node.default['rs-storage']['device']['mount_point']}"
 
 end
