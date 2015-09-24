3572f0f57d2b311482f3da3d89fb65d560eac64a
gonzalez <edds718@gmail.com>
2015-09-24 13:57:01 -0400

    fixing dbpath config option

--- recipes/restore_from_backups.rb
+++ recipes/restore_from_backups.rb
@@ -10,6 +10,6 @@ include_recipe 'rightscale_volume::default'
 include_recipe 'rs-storage::volume'
 
 #when using volumes we set the datadir to the mount point.
-node.default[:mongodb][:config][:datadir] = "#{node.default['rs-storage']['device']['mount_point']}"
+node.default[:mongodb][:config][:dbpath] = "#{node.default['rs-storage']['device']['mount_point']}"
 
 node.default[:rs-storage][:restore][:lineage] = "#{node['rsc_mongodb']['restore_lineage_name']}"
