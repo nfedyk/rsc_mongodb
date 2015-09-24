70b14055024e4bbcf9c3894446421f6f3f3fb058
gonzalez <edds718@gmail.com>
2015-09-24 14:21:27 -0400

    fixing config names

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,20 +32,8 @@ include_recipe 'mongodb::mongodb_org_repo'
 include_recipe 'machine_tag::default'
 
 
-node.default[:mongodb][:config][:replset] = "#{node[:rsc_mongodb][:replicaset]}"
-
-# if node['rsc_mongodb']['use_storage'] == 'true'
-# Chef::Log.info "Volumes are being used. Waiting for volume to be mounted"
-# ruby_block 'wait for volumes' do
-#   block do
-#     true until ::File.exists?('/var/lib/mongodb')
-#   end
-# end
-
 Chef::Log.info "Running the mongodb installer"
 include_recipe 'mongodb::default'
-
-# end
 
 
 #Tag host with replica set name
