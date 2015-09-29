d1ec5e068cacd6e15af93e62c6ce177dd46a69b8
gonzalez <edds718@gmail.com>
2015-09-29 10:14:40 -0400

    add to replicaset

--- recipes/add_to_replicaset.rb
+++ recipes/add_to_replicaset.rb
@@ -11,7 +11,7 @@ mongo_node = tag_search(node, "mongodb:replicaset=#{node[:rsc_mongodb][:replicas
 
    ip_address = mongo_node['server:private_ip_0'].first.value + ':27017'
    Chef::Log.info "#{ip_address}"
-   
+
 
 
 rs_config = rs_config.to_s + "     ]
@@ -22,7 +22,7 @@ Chef::Log.info "#{rs_config}"
 bash 'initiate the node' do
   code <<-EOH
     mongo <<CONFIG
-      rs.initiate(#{rs_config}.to_s);
+      rs.add(#{ip_address});
     CONFIG
   EOH
   flags '-xe'
