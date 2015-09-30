18612134a1c9c3fccb6f4b74d1216eccc92a897c
gonzalez <edds718@gmail.com>
2015-09-30 16:16:26 -0400

    find master code

--- recipes/add_to_replicaset.rb
+++ recipes/add_to_replicaset.rb
@@ -19,18 +19,23 @@ replicaset_hosts.each do | server |
 
 end
 
+#find primary
+#{primary_mongo_node} = mongo --quiet --eval "db.isMaster()['primary']"
 
-# Chef::Log.info "#{rs_config}"
-# ## initiate replica set , replica set name is already in the config
-# bash 'initiate the node' do
-#   code <<-EOH
-#     mongo <<CONFIG
-#       rs.add(#{ip_address});
-#     CONFIG
-#   EOH
-#   flags '-xe'
-# end
-#
-# machine_tag "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}" do
-#    action :create
-# end
+Chef::Log.info "#{rs_config}"
+## initiate replica set , replica set name is already in the config
+bash 'initiate the node' do
+  code <<-EOH
+    mongo --host #{node[:rsc_mongodb][:replicaset]}/#{ip_address}<<CONFIG
+      rs.add(#{ip_address});
+    CONFIG
+  EOH
+  flags '-xe'
+end
+
+machine_tag "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}" do
+   action :create
+end
+
+#connects to primary everytime.
+#mongo --host my_repl_set_name/my_mongo_server1
