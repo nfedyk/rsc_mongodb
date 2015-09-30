3740eeb6f157737c832ee1db7e61e479ee7f34ba
gonzalez <edds718@gmail.com>
2015-09-30 18:01:25 -0400

    find master code

--- recipes/add_to_replicaset.rb
+++ recipes/add_to_replicaset.rb
@@ -9,7 +9,7 @@ Chef::Log.info 'Searching for mongodb nodes'
 replicaset_hosts = tag_search(node, "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}")
 
 ip_address = replicaset_hosts[1]['server:private_ip_0'].first.value
-Chef::Log.info "#{ip_address}"
+Chef::Log.info "Host ip: #{ip_address}"
 
 #   $ip_address = server['server:private_ip_0'].first.value + ':27017'
 
@@ -19,19 +19,19 @@ Chef::Log.info "#{ip_address}"
 #find primary
 #{primary_mongo_node} = mongo --quiet --eval "db.isMaster()['primary']"
 
-# ## initiate replica set , replica set name is already in the config
-# bash 'initiate the node' do
-#   code <<-EOH
-#     mongo --host #{node[:rsc_mongodb][:replicaset]}/#{$ip_address}<<CONFIG
-#       rs.add("#{node[:cloud][:private_ips][0]}");
-#     CONFIG
-#   EOH
-#   flags '-xe'
-# end
-#
-# machine_tag "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}" do
-#    action :create
-# end
+## initiate replica set , replica set name is already in the config
+bash 'initiate the node' do
+  code <<-EOH
+    mongo --host #{node[:rsc_mongodb][:replicaset]}/#{$ip_address}<<CONFIG
+      rs.add("#{node[:cloud][:private_ips][0]}");
+    CONFIG
+  EOH
+  flags '-xe'
+end
+
+machine_tag "mongodb:replicaset=#{node[:rsc_mongodb][:replicaset]}" do
+   action :create
+end
 
 #connects to primary everytime.
 #mongo --host my_repl_set_name/my_mongo_server1
