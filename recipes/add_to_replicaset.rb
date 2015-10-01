8e32f08dc7da27f66a8c81f1a38a3fee4eaa95e6
gonzalez <edds718@gmail.com>
2015-10-01 13:51:47 -0400

    private ip reference changed

--- recipes/add_to_replicaset.rb
+++ recipes/add_to_replicaset.rb
@@ -34,7 +34,7 @@ Chef::Log.info "Node's Current IP: #{node['cloud']['private_ips'][0]}"
 bash 'initiate the node' do
   code <<-EOH
     mongo --host #{node[:rsc_mongodb][:replicaset]}/#{ip_address}<<CONFIG
-      rs.add("#{node['cloud']['private_ips'][0]}");
+      rs.add(#{node['cloud']['private_ips'][0]});
     CONFIG
   EOH
   flags '-xe'
