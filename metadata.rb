bf517b0981236d433823e6c828a818b7cfd73ee9
gonzalez <edds718@gmail.com>
2015-10-06 15:55:08 -0400

    fix version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'premium@rightscale.com'
 license          'Apache 2.0'
 description      'Installs/Configures Mongo DB'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.'
+version          '1.0'
 
 depends 'build-essential'
 depends 'mongodb'
